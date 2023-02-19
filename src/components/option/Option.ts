8bc243f369cfe39b894fccabd71807693ca4a249
Nora Mate <norama.matema@gmail.com>
2023-02-19 21:40:49 +0100

    IStyle.multiSelectContainer

--- src/components/option/Option.ts
+++ src/components/option/Option.ts
@@ -3,6 +3,7 @@ import { JSX } from 'solid-js'
 export type IOption = Record<string, string | number | boolean | unknown> | string | number
 
 export type IStyle = {
+  multiSelectContainer?: JSX.CSSProperties
   optionListContainer?: JSX.CSSProperties
   selectedListContainer?: JSX.CSSProperties
   optionContainer?: JSX.CSSProperties
